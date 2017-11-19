5848b6d315ebb18db210760af697f80ff799f117
francium <varunhooda1@gmail.com>
2017-11-18 19:07:44 -0500

    Test TransferClient functionality.
    Test ensures TransferClient is able to poll at the spcified frequency,
    is calling correct method on database interfacer, and is piping correct
    data to send server function.

--- dts_client/__main__.py
+++ dts_client/__main__.py
@@ -14,19 +14,18 @@ from .util import unix_time_to_human
 
 class TransferClient:
     def __init__(self,
-                 database_file,
-                 server_address,
+                 database_interfacer,
+                 server_send,
                  interval=DEFAULT_INTERVAL):
         '''
-        :param database_file:  Path to Weewx database
-        :param server_address: Address for server
-        :param interval:       Transfer interval (seconds)
+        :param database_interfacer:  An object that knows how to query the database
+        :param server_send:          Function to send data to a server
+        :param interval:             Transfer interval (seconds)
         '''
-        self._database_file = database_file
-        self._server_address = server_address
+        self._is_running = True
+        self._database_interfacer = database_interfacer
+        self._server_send = server_send
         self._interval = interval
-        self._db = weewx_orm.WeewxDB(database_file)
-
         self._last_query_time = time.time()
 
     def start(self):
@@ -34,7 +33,7 @@ class TransferClient:
         self._last_query_time = start_time
         time_now = start_time
         time.sleep(self._interval - (time_now - start_time) % self._interval)
-        while True:
+        while self._is_running:
             data = self._load_last_interval_data()
             self._transfer_data(data)
 
@@ -43,13 +42,16 @@ class TransferClient:
             time.sleep(self._interval -
                        (time_now - start_time) % self._interval)
 
+    def stop(self):
+        self._is_running = False
+
     def _load_last_interval_data(self):
         from_ = round(self._last_query_time)
         to = round(time.time())
         print('Querying data between {} and {}'.format(
             unix_time_to_human(from_), unix_time_to_human(to)
         ))
-        return self._db.archive_query_interval(from_, to)
+        return self._database_interfacer.archive_query_interval(from_, to)
 
     def _transfer_data(self, data):
         '''
@@ -57,11 +59,24 @@ class TransferClient:
         '''
         # Handle server unavailable
         # Serialize data
-        requests.post(self._server_address, data=data)
+        self._server_send(data)
+
+
+def server_send(server_address):
+    '''
+    :param server_address: Address for server
+    :throws: Network Exceptions
+    '''
+    def func(data):
+        requests.post(server_address, data=data)
+
+    return func
 
 
 if __name__ == '__main__':
     cli_args = args.parse()
-    client = TransferClient(cli_args.database, cli_args.server,
+    database_interfacer = weewx_orm.WeewxDB(cli_args.database)
+    client = TransferClient(database_interfacer,
+                            server_send(cli_args.server),
                             cli_args.interval)
     client.start()
---
+++ test/test_transfer_client.py
@@ -0,0 +1,49 @@
+import time
+import unittest
+from unittest import mock
+
+from dts_client.__main__ import TransferClient
+
+
+class Test_DTS_Client(unittest.TestCase):
+    def setUp(self):
+        self.setUp_server_send()
+        self.setUp_database_interfacer()
+        self.last_payload = None
+        self.last_interval = None
+        self.interval = 1
+        self.call_count = 0
+        self.expected_calls = 3
+        self.client = TransferClient(self.database_interfacer,
+                                     self.server_send,
+                                     self.interval)
+
+    def setUp_database_interfacer(self):
+        def mock_query(from_, to):
+            self.assert_interval(from_, to)
+            self.last_payload = dict(payload=int(time.time()))
+            return self.last_payload
+
+        dbi = mock.MagicMock()
+        self.database_interfacer = dbi
+        dbi.archive_query_interval.side_effect = mock_query
+
+    def setUp_server_send(self):
+        def mock_server_send(data):
+            self.call_count += 1
+            self.assertEqual(self.last_payload, data)
+            if (self.call_count >= self.expected_calls):
+                self.client.stop()
+
+        self.server_send = mock.MagicMock(side_effect=mock_server_send)
+
+    def test_start_interval(self):
+        self.client.start()
+        #  self.assertEqual(self.database_interfacer.call_count, self.expected_calls)
+        #  self.assertEqual(self.server_send.call_count, self.expected_calls)
+
+    def assert_interval(self, from_, to):
+        if (self.last_interval):
+            self.assertAlmostEqual(from_ - self.last_interval[1], 0, delta=0.1)
+
+        self.last_interval = (from_, to)
